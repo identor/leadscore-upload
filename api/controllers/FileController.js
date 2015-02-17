ddbb810629b84f392b86d99fecc3f60c79847abe
Jomar Uyaan <jomaruyaan@gmail.com>
2015-02-18 04:19:23 +0800

    Used mongodb insert instead of Model creation in inserting to the database.

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -7,71 +7,7 @@
 
 var csv = require('fast-csv');
 var MongoClient = require('mongodb').MongoClient;
-/*
-function upload(req, res) {
-  req.file('csv').upload({
-    dirname: '../../assets/csv',
-    maxBytes: 100000000
-  }, function (err, uploadedCsv) {
-    if (err) {
-      return res.serverError(err);
-    }
 
-    csv.fromPath(uploadedCsv[0].fd, {headers: true})
-      .on('data', function(data) {
-        var scorer = data['Scorer'];
-        var branch = '';
-        if (scorer.indexOf('ARPI5') != -1) {
-            branch = 'Dagupan';
-        } else if (scorer.indexOf('ARS') != -1){
-            branch = 'Others';
-        } else {
-            branch = 'Baguio';
-        }
-
-        var filename = uploadedCsv[0].filename;
-        var yearString = filename.slice(23, 27);
-        var monthString = filename.slice(27, 29);
-        var dayString = filename.slice(29, 31);
-        var fileDate = new Date(yearString + '-' + monthString + '-' + dayString);
-        var processingTime2 = parseInt(data['Processing Time(Sec)']);
-        if (processingTime2 === 'undefined') {
-          processingTime2 = 0;
-          console.log('Processing time was called undefined');
-        }
-        Score.create({
-          fileDate: fileDate,
-          callStartTime: data['Call Start Time'],
-          processingStart: data['Processing Start'],
-          processingEnd: data['Processing End'],
-          processingTime: processingTime2,
-          industry: data['Industry'],
-          branch: branch,
-          scorer: scorer,
-          accountName: data['Account Name'],
-          customerName: data['Customer Name'],
-          callId: data['Call ID'],
-          callType: data['Call Type'],
-          callStatus: data['Call Status'],
-          callDuration: data['Call Duration'],
-          url: data['Audio URL']
-        }, function (err, score) {
-          console.log(score.processingTime);
-          console.log(score.callId);
-          if (err) {
-            console.log('here');
-            return res.serverError(err);
-          }
-          return res.json(score);
-        });
-        res.json({
-          message: uploadedCsv.length + ' file uploaded sucessfully.',
-          csv: uploadedCsv
-        });
-      });
-  });
-}
-*/
 function upload(req, res) {
   req.file('csv').upload({
     dirname: '../../assets/csv',
@@ -146,7 +82,7 @@ function retrieve(req, res) {
           }
           averageProcessingTime = totalCallDuration / data.length;
           averageCallDuration = totalProcessingTime / data.length;
-          Leadscore.create({
+          var leadscore = {
             scorer: docs[index],
             totalProcessingTime: totalProcessingTime,
             totalCallDuration: totalCallDuration,
@@ -156,8 +92,10 @@ function retrieve(req, res) {
             industry: data[0].industry,
             callCount: data.length,
             date: data[0].fileDate,
-          }, function (err, leadscore) {
-              res.json(leadscore);
+          };
+          db.collection('leadscore').insert(leadscore, function (err, leadscore) {
+            if (err) throw err;
+            console.log(leadscore);
           });
         });
       };
