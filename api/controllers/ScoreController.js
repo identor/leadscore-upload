8df7c8f831a4b55d37a54f762dde86073e7e11c3
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2015-02-20 20:33:25 +0800

    Score stats introduced to reduce front-end processing load.

--- api/controllers/ScoreController.js
+++ api/controllers/ScoreController.js
@@ -67,7 +67,49 @@ function productivity(req, res) {
   MongoClient.connect('mongodb://localhost:27017/Leadscore', connected);
 }
 
+function stats(req, res) {
+  var logError = function (err) {
+    console.error(err);
+  };
+  var sendErrorResponse = function(err) {
+    if (err) {
+      logError(err);
+      res.status(500);
+      res.json(err);
+    }
+  };
+  var sendResponse = function(data) {
+    res.json(data);
+  };
+  var dataAggregated = function(err, aggregatedData) {
+    if (err) logError(err);
+    sendResponse(aggregatedData);
+  };
+  var connected = function (err, db) {
+    if (err) sendErrorResponse (err);
+    var scores = db.collection('score');
+    var aggregateGroup = {
+      _id: null,
+      scorers: { $addToSet: '$scorer' },
+      dates: { $addToSet: '$fileDate' },
+      branches: { $addToSet: '$branch' },
+      industries: { $addToSet: '$branch' },
+      totalCallDuration: { $sum: '$callDuration' },
+      totalProcessingTime: { $sum: '$processingTime' },
+      averageCallDuration: { $avg: '$callDuration' },
+      averageProcessingTime: { $avg: '$processingTime' },
+      callCount: { $sum: 1 }
+    };
+    var aggregateOpts = [{
+      $group: aggregateGroup
+    }]
+    scores.aggregate(aggregateOpts, dataAggregated);
+  };
+  MongoClient.connect('mongodb://localhost:27017/Leadscore', connected);
+}
+
 module.exports = {
-  productivity: productivity
+  productivity: productivity,
+  stats: stats
 };
 
